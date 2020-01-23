3cdc22b6fad091bee9d61452ecc9a595dfa51844
ahellerjones <44989975+ahellerjones@users.noreply.github.com>
2020-01-23 00:59:39 -0500

    Add files via upload

--- src/SailbotEncodedBLDC/SailbotEncodedBLDC.cpp
+++ src/SailbotEncodedBLDC/SailbotEncodedBLDC.cpp
@@ -3,22 +3,38 @@
 #include "../Encoder/Encoder.h"
 
 /** Class for controlling a BLDC motor w/ Encoder using PID control.
- * param initialEncoder's - Must be hardware interrupt pins.
- * param pwmSetSpeedPin - must be pwm enabled.
- * param forward / backPin - can be any digital output pins
- *
+ * REQUIRED: Arduino, L298N motor driver, BLDC motor w/ encoder, regulated 12V
+ * power supply
+ @param initialEncoderA - the first Encoder pin, must be interrupt enabled
+ * for the arduino uno, use pin 2
+ * @param initialEncoderB - the second Encoder pin, must be interrupt enabled
+ * for the arduino uno, use pin 3
+ * Connect initialEncoderA to the 'A' pin of the motor, and initialEncoderB to
+ * 'B'
+ @param pwmSetSpeedPin - This pin connects to EnA of the L298N. This is the
+ * enable pin for the driver. The PWM value sent to this pin will dictate the
+ * speed at which the motor connected to the 1st output of the driver
+ * will run.
+ @param forwardPin - This pin connects to IN1 of the driver. It is apart of the
+ * driver's directional control.
+ @param backPin - This pin connects to IN2 of the driver. It is apart of the
+ * driver's directional control.
+ * When forwardPin is HIGH & backPin is LOW, the motor is supplied a positive
+ * voltage. forwardPin = LOW & backPin = HIGH, a negative voltage.
+ * If both pins are low the motor recieves no voltage. Not sure what happens for
+ * both HIGH
  * See #TODO's in rotateToDegrees
  */
 EncodedBLDC::EncodedBLDC(
     int initialEncoderA,
       int initialEncoderB, int pwmSetSpeedPin, int forwardPin,
         int backPin) {
-  // Encoder initialization
+  // Motor encoder, uses standard Encoder library to count pulses made by
+  // The motor's encoder. initialEncoderA & initialEncoderB must be interrupts
   encoder = new Encoder(initialEncoderA, initialEncoderB);
-
+  //PID object, uses the PID library in the github. 
   thePID  = new PID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
   thePID->SetMode(AUTOMATIC);
-  //Adjust PID values
   thePID->SetTunings(Kp, Ki, Kd);
   thePID->SetSampleTime(1);
   thePID->SetOutputLimits(-maxSpeed, maxSpeed);
@@ -26,6 +42,9 @@ EncodedBLDC::EncodedBLDC(
 /** Sets the tunings for the PID. Must be reconfigured for ANY change to set up
  * by default these are set to 0 and must be changed in order to do anything
  * previously working non-load values are 7, 0, .05
+ @param newKp - the new proportional tuning to be applied to the algorithm
+ @param newKi - the new integral tuning to be applied to the algorithm
+ @param newKd - the new derivative tuning to be applied to the algorithm
  * See below link for help tuning.
  https://robotics.stackexchange.com/questions/167/what-are-good-strategies-for-tuning-pid-loops
  */
@@ -39,6 +58,21 @@ void EncodedBLDC::setTunings(double newKp, double newKi, double newKd) {
  * Serial print statements can be taken out, however there seems to be problems
  * without them as the provide a small delay that actually helps the algorithm
  * work. See if you can fix it.
+ *
+ * The PID algorithm takes in a Setpoint (numDeg) and maps the number of degrees
+ * To the number of pulses (from the encoder) that need to be counted for the
+ * motor to be in the correct position relative to the new Setpoint
+ * The PID thus calculates an Output, a PWM signal to apply to the motor, and
+ * then adjusts the PWM value based on the feedback by the encoder. When the
+ * motor gets close to the correct position it lessens the Output, even making
+ * it negative if the encoder shows that the motor has overshot the Setpoint.
+ * The while loop runs this feedback loop for a set amount of time, and the m
+ * otor generally settles within that alloted time. See the block comment to
+ * improve.
+ @param numDeg - the number of degrees to turn the BLDC. This value
+ can be anything... position/negative, integer/real. It also can be
+ greater than 360.
+
  */
 void EncodedBLDC::rotateToDegrees(int numDeg) {
   numPulse = map(numDeg, -360, 360, -1632, 1632) + encoder->read();
@@ -79,8 +113,9 @@ void EncodedBLDC::rotateToDegrees(int numDeg) {
   digitalWrite(inputBack, LOW);
 }
 /** private method to rotate the motor, only ever called by rotateToDegrees
- * param pwmOut - the pwm that dictates how fast the motor driver should output
- *
+ @param pwmOut - the pwm that dictates how fast the motor driver should rotate
+ * The digitalWrite()'s dictate to the driver the correct orientation to apply
+ * voltage to the motor.
  */
 void EncodedBLDC::rotate(int pwmOut){
   if (pwmOut > 0) {
